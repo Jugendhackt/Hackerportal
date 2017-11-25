9b87add05af489e1ae6c50ac62bac04463ecc69a
Mastermori <mastermoriii@gmail.com>
2017-11-25 10:51:48 +0100

    added index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html lang="de">
+
+	<head>
+		<title>Hackerportal</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width = device-width, initial-scale = 1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<script src="https://use.fontawesome.com/4e14f8df98.js"></script>
+  		<link rel="stylesheet" href="style.css">
+	</head>
+
+	<body>
+
+		<div id="main">
+
+		</div>
+
+		<script src="https://use.fontawesome.com/2beeaf0fcf.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	</body>
+
+</html>
