e9f6fcdcca8be4fdcc7f106f6525529aa9b9d850
Mondanzo <sackboysteve1@gmail.com>
2017-11-25 23:16:29 +0100

    Added ClearParser
    Removes every style from the text (for the cards)

--- Editor/parser.js
+++ Editor/parser.js
@@ -13,3 +13,15 @@ function bbcParse(text){
     text = text.replace(/\[\/strike\]/g,'</s>');
     return text;
 }
+
+function clearParse(text){
+    text = text.replace(/\[bold\]/g,'');
+    text = text.replace(/\[\/bold\]/g,'');
+    text = text.replace(/\[italic\]/g,'');
+    text = text.replace(/\[\/italic\]/g,'');
+    text = text.replace(/\[underline\]/g,'');
+    text = text.replace(/\[\/underline\]/g,'');
+    text = text.replace(/\[strike\]/g,'');
+    text = text.replace(/\[\/strike\]/g,'');
+    return text;
+}
