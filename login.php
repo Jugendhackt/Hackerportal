759049814fea2aa8d5b6d17e47e86fb3a05379bb
davidcolombogit <root@david-colombo.net>
2017-11-26 11:56:38 +0100

    update

--- login.php
+++ login.php
@@ -1,30 +1,11 @@
 <?php
-$email = $_POST['email'];
-$password = $_POST['password'];
+$ekey = $_POST['key'];
 
-$dbh = new PDO('mysql:host=10.23.41.183;dbname=hackerportal', 'root', 'mysqlpi01');
-
-$sth = $dbh->prepare("SELECT * FROM users WHERE email=?");
-$sth->execute([$email]);
-
-
-if($sth->errorCode() == 0) {
-    $user = $sth->fetch();
-    if ($user !== false) {
-        if (password_verify($password, $user['password'])) {
+if($key == "1234") {
             session_start();
             	if($_SESSION['permLvl'] == 5){
             		header('Location: dashboard.php', true, false ? 301 : 302);
                 	exit();
             	}
             echo "success";
-        }else{
-            echo "error";
-        }
-    }else{
-        echo "error2";
-    }
-}else{
-    echo "error3";
-}
 ?>
