5770db4ca1f2cb3b4d5aea75d0659fe642d5e3b0
davidcolombogit <root@david-colombo.net>
2017-11-26 12:01:10 +0100

    update

--- login.php
+++ login.php
@@ -3,10 +3,7 @@ $ekey = $_POST['key'];
 
 if ($key == "1234") {
     session_start();
-    if ($_SESSION['permLvl'] == 5) {
-        header('Location: dashboard.php', true, false ? 301 : 302);
-        exit();
-    }
+    $_SESSION['permLvl'] == 5;
     echo "success";
 } else {
     echo "error";
