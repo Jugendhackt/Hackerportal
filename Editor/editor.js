2e8981b99197db71e1e62024d4d388d24c8fbbc1
Mondanzo <sackboysteve1@gmail.com>
2017-11-26 11:01:47 +0100

    Added new Stuff and Tests
    Added new Code Type (Still WIP)
    Created tests and added based on the tests also alerts of bootstrap
    (still wip)

--- Editor/editor.js
+++ Editor/editor.js
@@ -3,9 +3,31 @@ var textForm = document.getElementById("editor-window-markdown");
 var title = document.getElementById("editor-title");
 var author = document.getElementById("editor-author");
 
-// Say in which Mode this Editor is (New or Edit Mode)
-var newMode = true;
-var articleId = null;
+/*
+ * Cookie Functions
+ */
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
 
 /*
  * Utilities Functions
@@ -25,10 +47,27 @@ const ajax = (action, data, callback) => {
     });
 };
 
+// Setup Editor (Disable Author auto input)
+
+// Say in which Mode this Editor is (New or Edit Mode)
+var newMode = true;
+var articleId = getCookie("articleId");
+console.log("Cookie: " + articleId);
+
+// Load Title
+if(articleId !== ""){
+    newMode = false;
+    title.value = "Loading...";
+    title.disabled = true;
+    ajax("getTitle", {articleId}, (result) => {
+        title.disabled = false;
+        title.value = result;
+    });
+}
+
 
 // Function to Autoresize the TextArea
 textForm.addEventListener("keydown", (event) => {
-    console.log("HI");
     textForm.style.height = 'auto';
     textForm.style.height = textForm.scrollHeight + "px";
 });
@@ -47,7 +86,7 @@ function insertSymbol(start, end, str, symbol1, symbol2){
 function BasicMarkupHandler(symbol1, symbol2, offset) {
     return function (event) {
         // Check if it needs to be offset (only for the old pos necessary)
-        if(offset == undefined)
+        if(offset === undefined)
             offset = 0;
 
         // Get Selected Area
@@ -70,16 +109,25 @@ $("#btn-bold").click(BasicMarkupHandler("[bold]", "[/bold]"));
 $("#btn-italic").click(BasicMarkupHandler("[italic]", "[/italic]"));
 $("#btn-underline").click(BasicMarkupHandler("[underline]", "[/underline]"));
 $("#btn-strike").click(BasicMarkupHandler("[strike]", "[/strike]"));
+$("#btn-code").click(BasicMarkupHandler("[code()]", "[/code]", -2));
+
+// Fonts Editor
 
 
-function submit(){
-    if(newMode){
-        ajax("create", {title: title.value, author: author.value, content: textForm.value}, (result) => {
-            console.log(result);
-        });
-    } else {
-        if(!articleId == null){
-
+// Final Function to submit it.
+function submit() {
+    if ($("#textForm").value() !== "" && $("#editor-author").value() !== "" && $("#editor-title").value() !== "") {
+        if (newMode) {
+            ajax("create", {title: title.value, author: author.value, content: textForm.value}, (result) => {
+                if (isNaN(result)) {
+                    window.location.pathname = "article.php?=" + result;
+                } else {
+                    $("#error-alert").alert();
+                    console.log(result);
+                }
+            });
         }
+    } else {
+        $("#error-alert-missing".alert());
     }
 }
--- Editor/parser.js
+++ Editor/parser.js
@@ -3,25 +3,28 @@
  */
 
 function bbcParse(text){
-    text = text.replace(/\[bold\]/g,'<b>');
-    text = text.replace(/\[\/bold\]/g,'</b>');
-    text = text.replace(/\[italic\]/g,'<i>');
-    text = text.replace(/\[\/italic\]/g,'</i>');
-    text = text.replace(/\[underline\]/g,'<u>');
-    text = text.replace(/\[\/underline\]/g,'</u>');
-    text = text.replace(/\[strike\]/g,'<s>');
-    text = text.replace(/\[\/strike\]/g,'</s>');
+    text = text.replace(/\[bold\]/gi,'<b>');
+    text = text.replace(/\[\/bold\]/gi,'</b>');
+    text = text.replace(/\[italic\]/gi,'<i>');
+    text = text.replace(/\[\/italic\]/gi,'</i>');
+    text = text.replace(/\[underline\]/gi,'<u>');
+    text = text.replace(/\[\/underline\]/gi,'</u>');
+    text = text.replace(/\[strike\]/gi,'<s>');
+    text = text.replace(/\[\/strike\]/gi,'</s>');
+    text = text.replace(/\[code()\]/gi,'<pre><code class="">');
+    text = text.replace(/\[\/code\]/gi,'</pre></code>');
     return text;
 }
 
 function clearParse(text){
-    text = text.replace(/\[bold\]/g,'');
-    text = text.replace(/\[\/bold\]/g,'');
-    text = text.replace(/\[italic\]/g,'');
-    text = text.replace(/\[\/italic\]/g,'');
-    text = text.replace(/\[underline\]/g,'');
-    text = text.replace(/\[\/underline\]/g,'');
-    text = text.replace(/\[strike\]/g,'');
-    text = text.replace(/\[\/strike\]/g,'');
+    text = text.replace(/\[bold\]/gi,'');
+    text = text.replace(/\[\/bold\]/gi,'');
+    text = text.replace(/\[italic\]/gi,'');
+    text = text.replace(/\[\/italic\]/gi,'');
+    text = text.replace(/\[underline\]/gi,'');
+    text = text.replace(/\[\/underline\]/gi,'');
+    text = text.replace(/\[strike\]/gi,'');
+    text = text.replace(/\[\/strike\]/gi,'');
+    text = text.replace(/\[code(java)\]/gi, '');
     return text;
 }
